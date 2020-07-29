c6994b7dfa110c66ed3f1afb9e873fe49e95dcb0
lecs01 <lecs.01@outlook.com>
2020-07-29 00:46:51 -0300

    NotaSong v1.1

--- app/src/main/java/com/example/nota_song/MainActivity.java
+++ app/src/main/java/com/example/nota_song/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nota_song;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button creditos = findViewById(R.id.buttonCreditos);
-
-        Intent intent = new Intent(getApplicationContext(),Creditos.class);
-        startActivity(intent);
+        creditos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Creditos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
